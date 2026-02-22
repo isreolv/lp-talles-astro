b741f29934523d48f7d2597b25a806d842a30b2e
Israel Oliveira <isroliveira@ici.tec.br>
2026-02-22 14:58:27 -0300

    feat: rework visuals

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,18 @@
 export const SITE_TITLE = 'Talles Cattarin';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'I\'m Talles Cattarin, a music producer dedicated to creating powerful, high-quality sound through modern mixing and mastering.';
+
+export const SOCIAL_INSTAGRAM = 'https://www.instagram.com/tallescattarin/';
+export const SOCIAL_YOUTUBE = 'https://www.youtube.com/@tallescattarin?sub_confirmation=1';
+export const SOCIAL_YOUTUBE_MUSIC = 'https://music.youtube.com/channel/UCsBFSXfuJxWPJjCHImM_Jsw';
+export const SOCIAL_SPOTIFY = 'https://open.spotify.com/artist/77SAbQybNSZmuluK93v8AO';
+export const SOCIAL_APPLE_MUSIC = 'https://music.apple.com/br/artist/talles-cattarin/1510095525';
+export const SOCIAL_DEEZER = 'https://www.deezer.com/br/artist/93422732';
+export const SOCIAL_AMAZON_MUSIC = 'https://music.amazon.de/artists/B0881KN65C/talles-cattarin';
+
+export const SPOTIFY_EMBED = 'https://open.spotify.com/embed/artist/77SAbQybNSZmuluK93v8AO?utm_source=generator&theme=0';
+
+export const CONTACT_WHATSAPP = 'https://api.whatsapp.com/send/?phone=5541998027818&text=Hi%20Talles%2C%20I%20found%20you%20on%20your%20website%20and%20I%27m%20interested%20in%20learning%20more%20about%20your%20services.&type=phone_number&app_absent=0';
+export const CONTACT_EMAIL = 'mailto:talles2_cattarin2@outlook.com';
+
+export const DONATION_PATREON = 'https://www.patreon.com/cw/tallescattarin';
+export const DONATION_PAYPAL = 'https://www.paypal.com/donate/?hosted_button_id=7N38B2WJJ9WHC';
