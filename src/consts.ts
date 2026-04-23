d100e46cf1fd98c23bd8dab183a5dbab3e81ae88
Israel Efraim de Oliveira <israolive@outlook.com>
2026-04-22 23:23:29 -0300

    feat: wip some more work

--- src/consts.ts
+++ src/consts.ts
@@ -4,11 +4,13 @@ export const SITE_DESCRIPTION = 'I\'m Talles Cattarin, a music producer dedicate
 export const SOCIAL_INSTAGRAM = 'https://www.instagram.com/tallescattarin/';
 export const SOCIAL_YOUTUBE = 'https://www.youtube.com/@tallescattarin?sub_confirmation=1';
 export const SOCIAL_TIKTOK = 'https://www.tiktok.com/@talles_cattarin';
-export const SOCIAL_YOUTUBE_MUSIC = 'https://music.youtube.com/channel/UCsBFSXfuJxWPJjCHImM_Jsw';
 export const SOCIAL_SPOTIFY = 'https://open.spotify.com/artist/77SAbQybNSZmuluK93v8AO';
-export const SOCIAL_APPLE_MUSIC = 'https://music.apple.com/br/artist/talles-cattarin/1510095525';
 export const SOCIAL_DEEZER = 'https://www.deezer.com/br/artist/93422732';
+export const SOCIAL_YOUTUBE_MUSIC = 'https://music.youtube.com/channel/UCsBFSXfuJxWPJjCHImM_Jsw';
+export const SOCIAL_APPLE_MUSIC = 'https://music.apple.com/br/artist/talles-cattarin/1510095525';
 export const SOCIAL_AMAZON_MUSIC = 'https://music.amazon.de/artists/B0881KN65C/talles-cattarin';
+export const SOCIAL_AIRBIT = 'https://www.airbit.com/tallescattarin';
+export const SOCIAL_FIVERR = 'https://www.fiverr.com/tallestc2001';
 
 export const SPOTIFY_EMBED = 'https://open.spotify.com/embed/artist/77SAbQybNSZmuluK93v8AO?utm_source=generator&theme=0';
 
