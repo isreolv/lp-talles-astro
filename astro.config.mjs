0aff7cfe1921b385954ddec3fc71e107b8c19413
Israel Efraim de Oliveira <israolive@outlook.com>
2026-04-25 22:41:51 -0300

    fix: astro config for site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://lp-talles-astro.pages.dev',
 	integrations: [mdx(), sitemap()],
 	vite: {
 		plugins: [tailwindcss()],
